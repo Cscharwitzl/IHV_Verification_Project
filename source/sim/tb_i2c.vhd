3fc595e919df5e76a23789920ff237e3be55084a
Clemens SCHARWITZL <cscharwitzl@lab8.ecslab.tuwien.ac.at>
2024-06-14 12:39:07 +0200

    fix bugs in i2c_vu add reading testcases, read should work

--- source/sim/tb_i2c.vhd
+++ source/sim/tb_i2c.vhd
@@ -8,6 +8,7 @@ architecture tb_i2c_arc of dut_test_ctrl is
   signal test_start : integer_barrier;
   signal test_done  : integer_barrier;
 
+
   function SetupControlReg(
       go       : boolean;
       len      : integer;
@@ -29,6 +30,10 @@ architecture tb_i2c_arc of dut_test_ctrl is
     return reg;
   end function;
 
+  shared variable data : std_logic_vector(64*8-1 downto 0) := (others => '0');
+  shared variable dev_addr      : std_logic_vector(6 downto 0);
+  shared variable reg_addr      : std_logic_vector(7 downto 0);
+
 begin
 
   CreateClock(clk_o, 10 ns);
@@ -37,7 +42,7 @@ begin
   stimuli_p: process is
     variable addr      : std_logic_vector(6 downto 0);
     variable byte_en   : std_logic_vector(3 downto 0);
-    variable data : DataRegArrayT(15 downto 0) := (others => (others => '0'));
+    variable datareg : DataRegArrayT(15 downto 0) := (others => (others => '0'));
   begin
 
     wait until rst_o = '0';
@@ -46,42 +51,80 @@ begin
 
     Log("*** Start of Tests (AVMM) ***");
 
-    data(0) := x"22_FF_AA_55";
-    startI2CTransfereInAVMM(avmm_trans_io,'0',3,x"AA","1010101",1,data);
-    
+    -- read 1 byte
+    datareg(0) := x"00_00_00_55";
+    data := DataRegArr_to_slv(datareg);
+    dev_addr := "1010101";
+    reg_addr := x"AA";
+    startI2CTransfereInAVMM(avmm_trans_io,'0',3,reg_addr,dev_addr,1,datareg);
     WaitForBarrier(test_done);
 
-    startI2CTransfereInAVMM(avmm_trans_io,'0',3,x"55","0101010",2,data);
+    -- read 4 byte
+    datareg(0) := x"22_FF_AA_55";
+    data := DataRegArr_to_slv(datareg);
+    dev_addr := "0101010";
+    reg_addr := x"55";
+    startI2CTransfereInAVMM(avmm_trans_io,'0',3,reg_addr,dev_addr,4,datareg);
+    WaitForBarrier(test_done);
+
+
+    --read wrong length slave
+    datareg(0) := x"22_FF_AA_55";
+    data := DataRegArr_to_slv(datareg);
+    dev_addr := "0101010";
+    reg_addr := x"55";
+    startI2CTransfereInAVMM(avmm_trans_io,'0',3,reg_addr,dev_addr,5,datareg);
+    WaitForBarrier(test_done);
+
+    --read wrong length master
+    datareg(0) := x"22_FF_AA_55";
+    data := DataRegArr_to_slv(datareg);
+    dev_addr := "0101010";
+    reg_addr := x"55";
+    startI2CTransfereInAVMM(avmm_trans_io,'0',3,reg_addr,dev_addr,4,datareg);
+    WaitForBarrier(test_done);
+    
     Log("*** End of Tests (AVMM) ***");
     Log("*** End of Testbench ***");
-
-    WaitForBarrier(test_done);
 
     std.env.stop;
   end process;
 
   read_p: process is
-    variable addr      : std_logic_vector(6 downto 0);
-    variable data      : std_logic_vector(63 downto 0);
-    variable data_read : std_logic_vector(63 downto 0);
+    variable data_read : std_logic_vector(64*8-1 downto 0) := (others => '0');
   begin
     WaitForBarrier(test_start);
 
     Log("*** Start of Tests (I2C) ***");
 
-    -- PoC
-    addr := "0100000";
-    data := x"00_00_00_00_22_FF_AA_55";
-    I2CRead(i2c_trans_io(3), addr, data_read);
-    AffirmIfEqual(data_read, data, "Test failed for addr " & to_hstring(addr));
-
+    -- read 1 byte
+    I2CRead(i2c_trans_io(3), x"00", data_read,1);
+    AffirmIfEqual(data_read, data, "Test failed for addr " & to_hstring(dev_addr));
+    AffirmIfEqual(std_logic_vector(i2c_trans_io(3).Address),dev_addr & reg_addr, "Test failed for addr " & to_hstring(dev_addr));
     WaitForBarrier(test_done);
     test_done <= 1;
 
-    I2CRead(i2c_trans_io(3), addr, data_read);
-    AffirmIfEqual(data_read, data, "Test failed for addr " & to_hstring(addr));
-
+    --read 4 byte
+    I2CRead(i2c_trans_io(3), x"00", data_read,4);
+    AffirmIfEqual(data_read, data, "Test failed for addr " & to_hstring(dev_addr));
+    AffirmIfEqual(std_logic_vector(i2c_trans_io(3).Address),dev_addr & reg_addr, "Test failed for addr " & to_hstring(dev_addr));
     WaitForBarrier(test_done);
+    test_done <= 1;
+
+    --read wrong length slave
+    I2CRead(i2c_trans_io(3), x"00", data_read,4);
+    AffirmIfEqual(data_read, data, "Test failed for addr " & to_hstring(dev_addr));
+    AffirmIfEqual(std_logic_vector(i2c_trans_io(3).Address),dev_addr & reg_addr, "Test failed for addr " & to_hstring(dev_addr));
+    WaitForBarrier(test_done);
+    test_done <= 1;
+
+    --read wrong length master
+    I2CRead(i2c_trans_io(3), x"00", data_read,5);
+    AffirmIfEqual(data_read, data, "Test failed for addr " & to_hstring(dev_addr));
+    AffirmIfEqual(std_logic_vector(i2c_trans_io(3).Address),dev_addr & reg_addr, "Test failed for addr " & to_hstring(dev_addr));
+    WaitForBarrier(test_done);
+    test_done <= 1;
+
 
     Log("*** End of Tests (I2C) ***");
 
--- source/sim/wave.do
+++ source/sim/wave.do
@@ -1,13 +1,15 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
+add wave -noupdate -divider <NULL>
 add wave -noupdate /dut_harness/avmm_trans_s
 add wave -noupdate /dut_harness/avmm_pins_s
 add wave -noupdate /dut_harness/i2c_trans_s
 add wave -noupdate /dut_harness/scl_s
-add wave -noupdate /dut_harness/sda_s
+add wave -noupdate -expand /dut_harness/sda_s
 add wave -noupdate -divider <NULL>
+add wave -noupdate /dut_harness/i2c_vu_gen(3)/i2c_vu_inst/sequencer_p/data
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {66921997 ps} 0}
+WaveRestoreCursors {{Cursor 1} {3105910000 ps} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 384
 configure wave -valuecolwidth 100
@@ -23,4 +25,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ns
 update
-WaveRestoreZoom {1501250525 ps} {2026471025 ps}
+WaveRestoreZoom {0 ps} {5155678500 ps}
