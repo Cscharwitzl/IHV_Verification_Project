afb354de6e1dab0f72c8ce9334a116645506e0a4
Clemens Scharwitzl <clemens@scharwitzl.com>
2024-06-16 15:49:04 +0200

    try writing tb

--- source/sim/tb_i2c_interrupt.vhd
+++ source/sim/tb_i2c_interrupt.vhd
@@ -9,6 +9,8 @@ architecture tb_i2c_interrupt_arc of dut_test_ctrl is
 
   signal SB : ScoreBoardIDType;
   signal tb_start, tb_end, test_start, test_end : integer_barrier;
+
+  variable datareg  : DataRegArrayT(15 downto 0)   := (others => (others => '0'));
 begin
 
   CreateClock(clk_o, 10 ns);
@@ -20,9 +22,19 @@ begin
     Log("*** Start of Testbench ***");
     wait for rst_o = '0';
     WaitForBarrier(tb_start);
-    -- Tests here -- 
+
+    --interrupt at transfer without error
+    datareg(7 downto 0) := x"55";
     WaitForBarrier(test_start);
+    startI2CTransfereInAVMM(avmm_trans_io,'0',0,x"AA","1111111",1,datareg);
     WaitForBarrier(test_end);
+    waitForFlags(avmm_trans_io,x"00",x"80000000",'0',CLK_DIVIDE_G);
+    AffirmIfEqual(irq_i, '1', "Interrupt output is not set");
+    AvmmRead(avmm_trans_io,x"01","0001",datareg(15));
+    AffirmIfEqual(datareg(15)(0),'1',"Interrupt status is not set");
+
+
+    --interrupt at transfer with error
 
     WaitForBarrier(tb_end);
     Log("*** End of Testbench ***");
